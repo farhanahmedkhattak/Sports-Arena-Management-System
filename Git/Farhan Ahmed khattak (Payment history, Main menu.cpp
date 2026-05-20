ecc94bd173a18837015b2975a4e747e47fd9c517
Farhan Ahmed Khattak <farhanmkhattak@gmail.com>
2026-05-20 23:36:28 +0500

    Fix formatting in Payment history, Main menu.cpp

--- Git/Farhan Ahmed khattak (Payment history, Main menu.cpp
+++ Git/Farhan Ahmed khattak (Payment history, Main menu.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-// ========================= PAYMENT CLASS =========================
+// ========================= PAYMENT CLASS ========================= 
 class Payment
 {
 private:
